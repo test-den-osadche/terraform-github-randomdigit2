2f0bc4471f3d0f2f2a5dc990241b3c289207ec71
test-den-osadche <164888207+test-den-osadche@users.noreply.github.com>
2024-03-29 14:16:58 +0200

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,5 +1,15 @@
-variable "prefix" {
-  description = "Prefix for the resource names"
-  type        = string
-  default     = "random"
+variable "vpc_id" {
+  type = string
+  description = "The ID of the VPC where resources will be created"
+}
+
+variable "subnet_id" {
+  type = string
+  description = "The ID of the subnet where resources will be created"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+  description = "The type of instance to be created (default: t2.micro)"
 }
